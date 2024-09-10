42d99b49d7e1c88268f1a267b5f20b10e43c7e78
MDMUHSINn <149122179+MDMUHSINn@users.noreply.github.com>
2024-09-10 18:43:49 +0300

    pw.js

--- util/pw.js
+++ util/pw.js
@@ -1,6 +1,6 @@
-const status = "AUTOMATIC";
-const botName = "💎 PRIME MUSIC";
-const Text = "GlaceYT";
+const status = "/help";
+const botName = "SONIC Player";
+const Text = "Spella";
 const version = "Latest@ v1.0"; 
 const startTime = Date.now();
 
