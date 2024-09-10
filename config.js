457ba10b5daeb37bfcf77a722635c2329aeef068
MDMUHSINn <149122179+MDMUHSINn@users.noreply.github.com>
2024-09-10 18:33:55 +0300

    config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["921743618316836875", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
