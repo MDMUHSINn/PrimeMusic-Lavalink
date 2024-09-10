a3ae1c938df99fc895ede61062b32bf8a9c0faf2
MDMUHSINn <149122179+MDMUHSINn@users.noreply.github.com>
2024-09-10 17:59:14 +0300

    support.js

--- commands/support.js
+++ commands/support.js
@@ -29,16 +29,19 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/uwVGgwHV3T";
+      const supportServerLink = "https://discord.gg/QHzdUj2VqN";
+      const githubLink = "https://github.com/GlaceYT";
+      const replitLink = "https://replit.com/@GlaceYT";
+      const youtubeLink = "https://spellaexp.my.canva.site/web";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              url: 'https://discord.gg/uwVGgwHV3T'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Website - ${youtubeLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1273947742724685875/1277995152543055932/0DC0BBD1-3E1D-4BA5-94E4-FA16355D48CA.jpg?ex=66e1a63c&is=66e054bc&hm=9bbdc07bebf6afa5963b8182b7e2963039a2f4b2f825af372ba53d20e9acb0ac&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
