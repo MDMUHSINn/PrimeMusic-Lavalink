9d79d0377976d897989d391511ba08aea7882fe2
MDMUHSINn <149122179+MDMUHSINn@users.noreply.github.com>
2024-09-10 17:31:04 +0300

    support.js

--- commands/support.js
+++ commands/support.js
@@ -29,10 +29,7 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const supportServerLink = "https://discord.gg/uwVGgwHV3T";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
