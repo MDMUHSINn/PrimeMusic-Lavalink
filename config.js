048ab2855fd1c9c225b14045c3add9d795b973b8
MDMUHSINn <149122179+MDMUHSINn@users.noreply.github.com>
2024-09-23 12:31:16 +0300

    config.js

--- config.js
+++ config.js
@@ -40,13 +40,13 @@ module.exports = {
 
   nodes: [
     {
-          name: "Node #1",
+       name: "Node #1",
         host: "lava-v3.ajieblogs.eu.org",
         port: 443,
         password: "https://dsc.gg/ajidevserver",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
-        secure: true
+        secure: false
     },
  ]
 }
